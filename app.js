b811136bf307bf13de75cc97eb96f8297668639e
priyanshu001 <priyanshurajput822400@gmail.com>
2022-09-13 22:31:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const app = express();
 
 const db = require("./config//database.js");
 require('dotenv').config()
-const {createProduct, getAllProducts} = require('./controllers/productController'); 
+const {createProduct, getAllProducts,deleteProduct, updateProduct, productDetails} = require('./controllers/productController'); 
 
 const port = process.env.PORT || 5000;
 
+const cors = require('cors')
+
 
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("https://apiiiiiiiiiiiiiiiii.herokuapp.com/createProduct for creating a product and https://apiiiiiiiiiiiiiiiii.herokuapp.com/getProduct for get all products");
-});
+
+app.use(cors())
 
 app.post("/createProduct",createProduct);
-app.get("/getProduct",getAllProducts);
+app.get("/",getAllProducts);
+app.get("/productDetails",productDetails);
+app.delete("/deleteProduct",deleteProduct);
+app.put("/updateProduct",updateProduct);
 
 app.listen(port,(err)=>{
     if(err){
--- controllers/productController.js
+++ controllers/productController.js
@@ -2,24 +2,85 @@
 const Product = require('../models/productModel.js')
 
 exports.getAllProducts = async (req, res, next) => {
+  console.log("Inside get all product")
+  const products = await Product.find();
+  // console.log("Products", products);
 
-    const products = await Product.find();
-    console.log("Products", products);
- 
-     res.status(201).json({
-         success: true,
-         products,
-         
- 
-     })
- };
+  return res.status(201).json({
+    success: true,
+    products,
+
+
+  })
+};
 exports.createProduct = (async (req, res, next) => {
-    // req.body.user = req.user.id;
-    // console.log(req.body.user);
-    const product = await Product.create(req.body);
+  // req.body.user = req.user.id;
+  console.log(req.body);
+  const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        product
-    })
+  res.status(201).json({
+    success: true,
+    product
+  })
+
+});
+
+exports.productDetails = async(req,res,next) => {
+  console.log("Product detials");
+  console.log(req.query.id);
+  Product.findOne({ _id: req.query.id })
+    .exec((err, product) => {
+      if (err || !product) {
+        console.log("ERRR632082a2bbc8971baa93268f",err);
+        return res.json({
+          error: err,
+        });
+      }
+       console.log(product);
+        return  res.json({
+          post : product
+    }); 
+        
+       
+        
+    });
+ }
+
+exports.deleteProduct = (async (req, res, next) => {
+  console.log("Inside delete");
+  console.log(req.query);
+
+  Product.findOne({ _id: req.query.id })
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.json({
+          error: err,
+        });
+      }
+
+      product
+        .remove()
+        .then((result) => {
+          res.json(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+    });
+})
+
+
+exports.updateProduct = (async (req, res, next) => {
+  console.log("Inside update")
+
+  Product.findByIdAndUpdate(req.query.id, req.body).
+    exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      else {
+        return res.json(result);
+      }
+    });
 });
